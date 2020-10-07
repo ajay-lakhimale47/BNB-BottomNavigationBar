4fdae0269b9c5c9f72f0a2dcae7bdb71514259ea
Ajay Lakhimale <aj47.bb4@gmail.com>
2020-10-07 09:47:44 +0530

    change color

--- lib/home.dart
+++ lib/home.dart
@@ -25,6 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         items: [
           BottomNavigationBarItem(
+            backgroundColor: Colors.deepOrange,
             icon: Icon(Icons.check_circle),
             label: 'Tasks',
           ),
